142843be7fd7d1a287f960d7beb230c56e62af97
Çağatay İnyurt <cagatayinyurt@outlook.com>
2022-03-07 19:47:34 +0300

    Update.

--- app/src/main/java/com/cagatayinyurt/foodapp/MainActivity.kt
+++ app/src/main/java/com/cagatayinyurt/foodapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.cagatayinyurt.foodapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
+
+//    override fun onStart() {
+//        super.onStart()
+//        setUpBottomNavigationMenu()
+//    }
+//
+//    private fun setUpBottomNavigationMenu() {
+//        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+//        val navController = findNavController(R.id.fragmentContainerView)
+//
+//        NavigationUI.setupWithNavController(bottomNavigation, navController)
+//    }
 }
